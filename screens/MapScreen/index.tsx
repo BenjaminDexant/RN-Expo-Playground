a3a9001dfccb318b22d07692d1aeef101e939199
benjamin <benjamin.d@make-it.fr>
2022-03-21 15:37:37 +0100

    add markers array to display in the map

--- screens/MapScreen/index.tsx
+++ screens/MapScreen/index.tsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
-import MapView from 'react-native-maps';
+import React, { useState, useRef } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import { RootTabScreenProps } from '../../types';
 
 const { height, width } = Dimensions.get('window');
 
 const MapScreen = ({ navigation }: RootTabScreenProps<'Todo'>) => {
+  const mapRef = useRef(null);
+  const [markers, setMarkers]= useState([{
+    coordinate: {
+      latitude: 43.604429,
+      longitude: 1.443348,
+    },
+    title: 'Toulouse',
+    description: 'Toulouse',
+  }]);
   const [region, setRegion] = useState({
-    latitude: 51.5079145,
-    longitude: -0.0899163,
+    latitude: 43.604429,
+    longitude: 1.443348,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
-  });
+});
   return (
     <View style={styles.container}>
       <MapView
+        ref={mapRef}
         style={styles.map}
         initialRegion={{
           latitude: 43.604429,
@@ -23,7 +33,16 @@ const MapScreen = ({ navigation }: RootTabScreenProps<'Todo'>) => {
           longitudeDelta: 0.01,
         }}
         onRegionChangeComplete={(region) => setRegion(region)}
-      />
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+        </MapView>
       <Text style={styles.text}>Current latitude: {region.latitude}</Text>
       <Text style={styles.text}>Current longitude: {region.longitude}</Text>
     </View>
