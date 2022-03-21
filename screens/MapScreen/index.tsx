db75102c96505351177ce43d43b17579b11fc88b
benjamin <benjamin.d@make-it.fr>
2022-03-21 15:16:28 +0100

    add default location and dynamic region lacation state with some css

--- screens/MapScreen/index.tsx
+++ screens/MapScreen/index.tsx
@@ -1,12 +1,31 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
 import { RootTabScreenProps } from '../../types';
 
+const { height, width } = Dimensions.get('window');
+
 const MapScreen = ({ navigation }: RootTabScreenProps<'Todo'>) => {
+  const [region, setRegion] = useState({
+    latitude: 51.5079145,
+    longitude: -0.0899163,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 43.604429,
+          longitude: 1.443348,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        onRegionChangeComplete={(region) => setRegion(region)}
+      />
+      <Text style={styles.text}>Current latitude: {region.latitude}</Text>
+      <Text style={styles.text}>Current longitude: {region.longitude}</Text>
     </View>
   );
 }
@@ -19,8 +38,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width,
+    height: height / 1.5,
+  },
+  text: {
+    fontSize: 10,
+    textAlign: 'center',
+    margin: 10,
   },
 });
 
